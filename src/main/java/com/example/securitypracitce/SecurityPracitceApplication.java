c16aaf6f319ea11d8b2c22f0e08df78d30863360
Jaehee-Ahn <jaehee1106@pincar.co.kr>
2022-01-04 11:20:48 +0900

    first commit

---
+++ src/main/java/com/example/securitypracitce/SecurityPracitceApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitypracitce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPracitceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPracitceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securitypracitce/SecurityPracitceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitypracitce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPracitceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
