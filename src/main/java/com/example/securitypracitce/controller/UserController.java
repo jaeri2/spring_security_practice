06949551291c5d047279a3eb443893c33bbd244e
Jaehee-Ahn <jaehee1106@pincar.co.kr>
2022-01-06 15:48:07 +0900

    로그인

--- src/main/java/com/example/securitypracitce/controller/UserController.java
+++ src/main/java/com/example/securitypracitce/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.example.securitypracitce.controller;
 
+import com.example.securitypracitce.domain.dto.LoginDto;
+import com.example.securitypracitce.domain.dto.TokenDto;
 import com.example.securitypracitce.domain.dto.UserDto;
+import com.example.securitypracitce.security.JwtFilter;
+import com.example.securitypracitce.security.TokenProvider;
 import com.example.securitypracitce.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +22,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @PostMapping("/join")
     public ResponseEntity<UserDto> signup(@RequestBody UserDto userDto) {
         return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody LoginDto loginDto) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwtToken = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwtToken);
+
+        return new ResponseEntity<>(new TokenDto(jwtToken), httpHeaders, HttpStatus.OK);
     }
 }
